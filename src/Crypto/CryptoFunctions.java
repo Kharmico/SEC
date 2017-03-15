772db276dbdc9349194233701ec085bac0889a10
Francisco Martins <franciscoprelhaz.m@gmail.com>
2017-03-15 15:45:41 +0000

    some crypto implemented

--- src/Crypto/CryptoFunctions.java
+++ src/Crypto/CryptoFunctions.java
@@ -2,6 +2,9 @@ package Crypto;
 
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
 import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
@@ -9,8 +12,13 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 
 public class CryptoFunctions {
+	
+	private PublicKey publicKey;
+	//private PrivateKey privateKey;
 	
 	public static String decrypt_data(String encData)
 	        throws NoSuchAlgorithmException, NoSuchPaddingException,
@@ -41,5 +49,23 @@ public class CryptoFunctions {
 
 	    byte[] original = Base64.getEncoder().encode(cipher.doFinal(data.getBytes()));
 	    return new String(original);
+	}
+	
+	public static byte[] sign_data(String data, PrivateKey key) throws InvalidKeyException, NoSuchAlgorithmException, SignatureException{
+		byte [] content = data.getBytes(); 
+		// generating a signature
+		Signature dsaForSign = Signature.getInstance("SHA1withDSA"); 
+		dsaForSign.initSign(key); 
+		dsaForSign.update(content); 
+		return dsaForSign.sign(); 
+		
+	}
+	
+	public static boolean verifySignature(byte[] data, byte[] signature, PublicKey key) throws InvalidKeyException, SignatureException, NoSuchAlgorithmException{
+		Signature dsaForVerify = Signature.getInstance("SHA1withDSA"); 
+		dsaForVerify.initVerify(key); 
+		dsaForVerify.update(data); 
+		return dsaForVerify.verify(signature); 
+		//System.out.println("Signature verifies: " + verifies); 
 	}
 }
