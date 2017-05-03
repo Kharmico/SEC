8f8d0fb16fa1ca5eadd31e8c1f065021b07fdea8
Kharmico <phoenix-2000@hotmail.com>
2017-05-03 19:42:56 +0100

    Script and file with input for clientapp
    Additional notes for script thinking were added, called "notes_for_script.txt"

---
+++ sec_proj.sh
@@ -0,0 +1,6 @@
+javac -sourcepath src -classpath lib/*:lib/jersey-bundle_lib/*:src/Crypto/*.class:src/Exceptions/*.class:src/Client/*.class:src/Server/*.class:. src/Server/*.java
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:./src/Client/:./src/Crypto/:./src/Exceptions/:. Server.Server 8000; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:./src/Client/:./src/Crypto/:./src/Exceptions/:. Server.Server 8500; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:./src/Client/:./src/Crypto/:./src/Exceptions/:. Server.Server 9000; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:./src/Client/:./src/Crypto/:./src/Exceptions/:. Server.Server 9500; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:./src/Client/:./src/Crypto/:./src/Exceptions/:. Client.ClientApp http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/ < ./fileForTest.txt; exec bash\""
