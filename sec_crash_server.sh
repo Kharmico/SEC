994edcd32387869f3ac9ff138985e6a7c573bb73
Kharmico <phoenix-2000@hotmail.com>
2017-05-04 19:29:03 +0100

    Changed scripts. Normal execution and crash 1 server

---
+++ sec_crash_server.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+javac -sourcepath src -classpath lib/*:lib/jersey-bundle_lib/*:src/Crypto/*.class:src/Exceptions/*.class:src/Client/*.class:src/Server/*.class:. src/Crypto/*.java src/Exceptions/*.java src/Server/*.java src/Client/*.java
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:. Server.Server 8000 http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:. Server.Server 8500 http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:. Server.Server 9000 http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:. Server.Server 9500 http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/; exec bash\""
+konsole -e "bash -c \"java -cp ./lib/*:./lib/Jersey-bundle_lib/*:./src/:. Client.ClientApp http://localhost:8000/ http://localhost:8500/ http://localhost:9000/ http://localhost:9500/ < ./fileForTest.txt; exec bash\""
+konsole --noclose -e "bash -c \"sleep 15; kill $(ps aux | grep "Server.Server 8500" | grep -v grep | awk '{print $2}' | awk '{print $0}'); exec bash\""
